1d9f07bf373e3cd0307c9c857f425d266838de97
YugandharVasudev <gyugdev@gmail.com>
2022-12-27 20:52:42 +0530

    charge

--- .history/Spotify Clone/script_20221227120416.js
+++ .history/Spotify Clone/script_20221227120416.js
@@ -19,19 +19,18 @@ let songs = [
 masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
-        // masterPlay.classList.remove('fa-play-circle');
-        // masterPlay.classList.add('fa-pause-circle');
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
     }
-    // else {
-    //     audioElement.pause();
-    //     masterPlay.classList.remove('fa-pause-circle');
-    //     masterPlay.classList.add('fa-play-circle');
-    // }
+    else {
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+    }
 })
 
 // Listen to Events
 myProgressBar.addEventListener('timeupdate', ()=>{
     console.log('timeupdate');
     // Update Seekbar
-
 });
--- .history/Spotify Clone/script_20221227121339.js
+++ .history/Spotify Clone/script_20221227121339.js
@@ -2,7 +2,7 @@ console.log("Welcome to Spotify");
 
 // Initilize the Variables
 let songIndex = 0;
-let audioElement = new Audio('songs/1.mp3');
+let audioElement = new Audio('1.mp3');
 let masterPlay = document.getElementById("masterPlay");
 let myProgressBar = document.getElementById("myProgressBar");
 let songs = [
@@ -19,19 +19,18 @@ let songs = [
 masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
-        // masterPlay.classList.remove('fa-play-circle');
-        // masterPlay.classList.add('fa-pause-circle');
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
     }
-    // else {
-    //     audioElement.pause();
-    //     masterPlay.classList.remove('fa-pause-circle');
-    //     masterPlay.classList.add('fa-play-circle');
-    // }
+    else {
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+    }
 })
 
 // Listen to Events
 myProgressBar.addEventListener('timeupdate', ()=>{
     console.log('timeupdate');
     // Update Seekbar
-
 });
